f7bf78fe9d590407c7e88f42f35e59d41bee3f25
Jason Koogler <jasonkoogler84@gmail.com>
2025-05-20 07:30:11 -0400

    basic tailwind config, added keyframes, utilities and mdx config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), mdx()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
