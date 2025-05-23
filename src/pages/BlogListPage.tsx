9ac8b04ec0f73af7347ae1c6dfb28729f2f7a516
Jason Koogler <jasonkoogler84@gmail.com>
2025-05-23 08:09:47 -0400

    blog list media queries

--- src/pages/BlogListPage.tsx
+++ src/pages/BlogListPage.tsx
@@ -31,29 +31,31 @@ const BlogListPage: React.FC = () => {
   ]);
 
   return (
-    <div className="flex flex-col  h-full text-green-500 p-4">
-      <h1 className="text-lg  mb-8 font-bold tracking-wider">BLOG ENTRIES</h1>
+    <div className="flex flex-col  h-full w-full text-green-500 p-6">
+      <div className='flex flex-col md:mx-auto md:w-2/3'>
 
-      <div className="grid gap-8 w-full">
-        {posts.map((post) => (
-          <article key={post.slug} className="border border-[#33ff33] p-5 hover:shadow-[0_0_12px_rgba(51,255,51,0.3)] transition-all">
-            <h2 className="text-xl font-bold mb-2">{post.title}</h2>
-            <div className="text-sm opacity-70 mb-3">
-              {new Date(post.date).toLocaleDateString('en-US', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-              })}
-            </div>
-            <p className="mb-4">{post.excerpt}</p>
-            <Link
-              to={`/blog/${post.slug}`}
-              className="inline-block px-4 py-2 border border-[#33ff33] hover:shadow-[0_0_8px_rgba(51,255,51,0.5)] transition-all"
-            >
-              READ POST &gt;
-            </Link>
-          </article>
-        ))}
+        <div className="grid gap-8 w-full md:items-center md:justify-center ">
+          <h1 className="text-lg  mb-8 font-bold tracking-wider">BLOG ENTRIES</h1>
+          {posts.map((post) => (
+            <article key={post.slug} className="border border-[#33ff33] p-5 hover:shadow-[0_0_12px_rgba(51,255,51,0.3)] transition-all">
+              <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+              <div className="text-sm opacity-70 mb-3">
+                {new Date(post.date).toLocaleDateString('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                })}
+              </div>
+              <p className="mb-4">{post.excerpt}</p>
+              <Link
+                to={`/blog/${post.slug}`}
+                className="inline-block px-4 py-2 border border-[#33ff33] hover:shadow-[0_0_8px_rgba(51,255,51,0.5)] transition-all"
+              >
+                READ POST &gt;
+              </Link>
+            </article>
+          ))}
+        </div>
       </div>
     </div>
   );
